5fe9eb3f42b645e649379ec33da7f61a21e9013e
中瑋 蔡 <107111248@mail.oit.edu.tw>
2021-03-23 15:13:12 +0800

    質數判斷

---
+++ 1HW/Prime.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _1HW
+{
+    public partial class Prime : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int num = 7;
+            string ans = " ";
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    ans = "NO";
+                    break;
+                }
+                else
+                {
+                    ans = "YES";
+                }
+            }
+            Response.Write(ans);
+        }
+    }
+}
